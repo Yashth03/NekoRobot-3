2cd4b1c0e4d53d1477ca35a922fd5b4462f9ddfc
Yashth03 <116712567+Yashth03@users.noreply.github.com>
2022-10-26 18:59:38 +0530

    Update disasters.py

--- NekoRobot/modules/disasters.py
+++ NekoRobot/modules/disasters.py
@@ -630,34 +630,20 @@ Group admins/group owners do not need these commands.
 Visit @{SUPPORT_CHAT} for more information.
 """
 
-SUDO_HANDLER = CommandHandler(("addsudo", "addarank"), addsudo, run_async=True)
-SUPPORT_HANDLER = CommandHandler(("addsupport", "addbrank"), addsupport, run_async=True)
-TIGER_HANDLER = CommandHandler(("addtiger", "addcrank"), addtiger, run_async=True)
-WHITELIST_HANDLER = CommandHandler(
-    ("addwhitelist", "adddrank"), addwhitelist, run_async=True
-)
-UNSUDO_HANDLER = CommandHandler(
-    ("removesudo", "removearank"), removesudo, run_async=True
-)
-UNSUPPORT_HANDLER = CommandHandler(
-    ("removesupport", "removebrank"), removesupport, run_async=True
-)
-UNTIGER_HANDLER = CommandHandler(
-    ("removetiger", "removecrank"), removetiger, run_async=True
-)
-UNWHITELIST_HANDLER = CommandHandler(
-    ("removewhitelist", "removedrank"), removewhitelist, run_async=True
-)
+SUDO_HANDLER = CommandHandler(("addsudo", "adddragon"), addsudo)
+SUPPORT_HANDLER = CommandHandler(("addsupport", "adddemon"), addsupport)
+TIGER_HANDLER = CommandHandler(("addtiger"), addtiger)
+WHITELIST_HANDLER = CommandHandler(("addwhitelist", "addwolf"), addwhitelist)
+UNSUDO_HANDLER = CommandHandler(("removesudo", "removedragon"), removesudo)
+UNSUPPORT_HANDLER = CommandHandler(("removesupport", "removedemon"), removesupport)
+UNTIGER_HANDLER = CommandHandler(("removetiger"), removetiger)
+UNWHITELIST_HANDLER = CommandHandler(("removewhitelist", "removewolf"), removewhitelist)
 
-WHITELISTLIST_HANDLER = CommandHandler(
-    ["whitelistlist", "dranks"], whitelistlist, run_async=True
-)
-TIGERLIST_HANDLER = CommandHandler(["tigers", "cranks"], tigerlist, run_async=True)
-SUPPORTLIST_HANDLER = CommandHandler(
-    ["supportlist", "branks"], supportlist, run_async=True
-)
-SUDOLIST_HANDLER = CommandHandler(["sudolist", "aranks"], sudolist, run_async=True)
-DEVLIST_HANDLER = CommandHandler(["devlist", "sranks"], devlist, run_async=True)
+WHITELISTLIST_HANDLER = CommandHandler(["whitelistlist", "wolves"], whitelistlist)
+TIGERLIST_HANDLER = CommandHandler(["tigers"], tigerlist)
+SUPPORTLIST_HANDLER = CommandHandler(["supportlist", "demons"], supportlist)
+SUDOLIST_HANDLER = CommandHandler(["sudolist", "dragons"], sudolist)
+DEVLIST_HANDLER = CommandHandler(["devlist", "devs"], devlist)
 
 NEKO_PTB.add_handler(SUDO_HANDLER)
 NEKO_PTB.add_handler(SUPPORT_HANDLER)
